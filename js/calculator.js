9edc0857f72da3ed446afba900b35f2ac2ff4ce2
rabbani4443 <rabbani07r@gmail.com>
2023-08-18 20:44:31 +0600

    finished the triangle card

---
+++ js/calculator.js
@@ -0,0 +1,62 @@
+// ==================  01. Triangle Card ================
+// ==================  01. Triangle Card ================
+function calculateTriangleArea(){
+    const base = getInputField ('triangle-base');
+    const height = getInputField ('triangle-hight');
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert number');
+        return;
+    }
+
+    const area = 0.5* base * height;
+    const areaTwoDecimal = area.toFixed(2)
+    setElementInnerText('triangle-area',areaTwoDecimal )
+
+    addToCalculationEntry(' Triangle :', areaTwoDecimal);
+
+}
+
+// ==================== 02. Rectangle Card ====================
+// ==================== 02. Rectangle Card ====================
+
+ function calculateRectangleArea(){
+
+ }
+
+
+// Get All Input Field Function 
+function getInputField(inputField){
+   const inputFieldId = document.getElementById(inputField);
+   const inputFieldText = inputFieldId.value;
+   const value = parseFloat(inputFieldText);
+   return value;
+}
+
+// Set Result on Area Element Section ( Area: 00 cm2 )
+
+function setElementInnerText(textElementId, areaResult ){
+    const element = document.getElementById(textElementId);
+    element.innerText = areaResult;
+}
+
+// add to calculation entry
+/**
+ * 1. get the element where you want to add the dynamic HTML
+ * 2. create an element you want to add
+ * 3. if needed add some class
+ * 4. set inner HTML. it could be dynamic Template string 
+ * 5. append the created element as a child of the parent
+ */
+
+function addToCalculationEntry(areaType, area){
+    
+    const calculateArea = document.getElementById('calculator-entry')
+    const p = document.createElement('p');
+
+    const count =calculateArea.childElementCount;
+    p.classList.add('pt-4', 'font-semibold')
+    p.innerHTML = `${count + 1 }. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>`;
+
+    calculateArea.appendChild(p);
+
+}
